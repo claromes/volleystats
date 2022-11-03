6b1a28d36c0ff02ea252fc8ef83ebd75770b06df
Claromes <claromes@hey.com>
2022-11-03 08:17:39 -0300

    update bash

--- superliga.sh
+++ superliga.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-FILE_NAME_HOME="home-superliga-$1.csv"
-FILE_NAME_GUEST="guest-superliga-$1.csv"
+FILE_NAME_HOME="data/home-superliga-$1.csv"
+FILE_NAME_GUEST="data/guest-superliga-$1.csv"
 
-if [ $2 ]
+if test $2
 then
-    FILE_NAME_HOME="$2-home-superliga-$1.csv"
-    FILE_NAME_GUEST="$2-guest-superliga-$1.csv"
+FILE_NAME_HOME="data/$2-home-superliga-$1.csv"
+FILE_NAME_GUEST="data/$2-guest-superliga-$1.csv"
 fi
 
-scrapy crawl home_superliga -a match=$1 -o data/$FILE_NAME_HOME &&
-scrapy crawl guest_superliga -a match=$1 -o data/$FILE_NAME_GUEST
+if test $3
+then
+FILE_NAME_HOME="data/round-$3/$2-home-superliga-$1.csv"
+FILE_NAME_GUEST="data/round-$3/$2-guest-superliga-$1.csv"
+fi
+
+scrapy crawl home_superliga -a match=$1 -o $FILE_NAME_HOME &&
+scrapy crawl guest_superliga -a match=$1 -o $FILE_NAME_GUEST
 
 exec bash
