c3c61334adf90b10340f73f77b864a99924fe21c
Claromes <claromes@hey.com>
2023-04-03 21:07:17 -0300

    scrap match list

---
+++ matchscraper/spiders/match_list.py
@@ -0,0 +1,35 @@
+import scrapy
+import os
+import re
+
+class MatchListSpider(scrapy.Spider):
+    name = 'match_list'
+
+    def __init__(self, fed_acronym='', competition_id='', **kwargs):
+        self.start_urls = [f'https://{fed_acronym}-web.dataproject.com/CompetitionMatches.aspx?ID={competition_id}']
+        self.competition_id = competition_id
+        match_id = ''
+        home_team = ''
+        guest_team = ''
+
+        super().__init__(**kwargs)
+
+    def parse(self, response):
+        for j in range(0, 60, 2):
+            for i in range(0, 12, 2):
+                match = response.xpath("//div[@id='ctl00_Content_Main_70_userControl_RADLIST_Legs_ctrl{}_RADLIST_Matches_ctrl{}_MatchRow']/div/div".format(j, i))
+
+                match_id_string = match.xpath("./div[4]/p/@onclick").get()
+                match_id = re.search(r'mID=(\d+)', match_id_string).group(1)
+
+                home_team = match.xpath("./div[5]/p/span/*/text() | ./div[5]/p/span/text()").get()
+                guest_team = match.xpath("./div[9]/p/span/*/text() | ./div[9]/p/span/text()").get()
+
+                yield {
+                    'Match ID': match_id,
+                    'Home Team': home_team,
+                    'Guest Team': guest_team
+                }
+
+    def closed(spider, reason):
+        os.rename('data/match_list.csv', 'data/match_list_{}.csv'.format(spider.competition_id))
