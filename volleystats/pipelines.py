1c356609a71a2e3679a66647fcc9bfeae9399e7b
Claromes <claromes@hey.com>
2023-09-23 17:19:07 -0300

    change project name

--- volleystats/pipelines.py
+++ volleystats/pipelines.py
@@ -8,6 +8,6 @@
 from itemadapter import ItemAdapter
 
 
-class MatchScraperPipeline:
+class VolleyStatsPipeline:
     def process_item(self, item, spider):
         return item
--- volleystats/settings.py
+++ volleystats/settings.py
@@ -1,4 +1,4 @@
-# Scrapy settings for matchscraper project
+# Scrapy settings for volleystats project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -7,14 +7,14 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'matchscraper'
+BOT_NAME = 'volleystats'
 
-SPIDER_MODULES = ['matchscraper.spiders']
-NEWSPIDER_MODULE = 'matchscraper.spiders'
+SPIDER_MODULES = ['volleystats.spiders']
+NEWSPIDER_MODULE = 'volleystats.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'matchscraper (+http://www.yourdomain.com)'
+#USER_AGENT = 'volleystats (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -45,13 +45,13 @@ ROBOTSTXT_OBEY = True
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'matchscraper.middlewares.MatchScraperSpiderMiddleware': 543,
+#    'volleystats.middlewares.VolleyStatsSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'matchscraper.middlewares.MatchScraperDownloaderMiddleware': 543,
+#    'volleystats.middlewares.VolleyStatsDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    'matchscraper.pipelines.MatchScraperPipeline': 300,
+#    'volleystats.pipelines.VolleyStatsPipeline': 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
